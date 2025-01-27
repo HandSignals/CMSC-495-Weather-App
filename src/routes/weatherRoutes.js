add0495343078be871a8ba8ba2ee315d702c5824
Scott Estes <scott.estes@aol.com>
2025-01-27 15:19:16 -0500

    Routes Updated

--- src/routes/weatherRoutes.js
+++ src/routes/weatherRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { fetchCurrentWeather, fetchForecastWeather } = require('../controllers/weatherController');
 
-router.get('/current', fetchCurrentWeather);
-router.get('/forecast', fetchForecastWeather);
+router.get('/api/weather/current', fetchCurrentWeather);
+router.get('/api/weather/forecast', fetchForecastWeather);
 
 module.exports = router;
