a6c331c467613dc8545f02202b91529cc41278b5
Daisy Sharma <daisysharma@Daisys-iMac.local>
2025-01-31 17:55:18 -0500

    front_end upload

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('/', (req, res) => {
-    res.send('Weather App API is running...');
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
 const weatherRoutes = require('./routes/weatherRoutes');
